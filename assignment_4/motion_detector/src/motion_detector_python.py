acd9b3f69bd502917673759708cf869c468abb7f
aamodh <aasuresh@ucsd.edu>
2016-10-24 19:52:41 -0700

    MOG2 successfully merged into main code

--- assignment_4/motion_detector/src/motion_detector_python.py
+++ assignment_4/motion_detector/src/motion_detector_python.py
@@ -10,7 +10,8 @@ from motion_detector.srv import ModeResponse
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-
+count=-1;
+bgs_mog = cv2.BackgroundSubtractorMOG2(50,20,True);
 class ImageConverter:
 
    def __init__(self):
@@ -22,6 +23,7 @@ class ImageConverter:
       self.bg_img_captured=False
 
    def FarnebackAlgorithm(self,grayscale_prev_img,grayscale_next_img,orig_next_img):
+      print('entered Farneback')
       flow_map = cv2.calcOpticalFlowFarneback(grayscale_prev_img, \
                      grayscale_next_img, 0.5, 3, 15, 3, 5, 1.2, 0)
       h, w = flow_map.shape[:2]
@@ -49,6 +51,46 @@ class ImageConverter:
       self.mode = req.mode
       return ModeResponse(True)
 
+   def MOG2(self,cv_image):
+      #global frame
+      global count
+      global bgs_mog
+      count=count+1
+      print(count)
+      frame=cv_image
+      
+      #bgs_mog = cv2.BackgroundSubtractorMOG2(50,20,True);
+      fgmask = bgs_mog.apply(frame)
+      kernel = np.ones((10,10), np.uint8)
+     # fgmask = cv2.dilate(fgmask, kernel, iterations=1)
+     # mask_rbg = cv2.cvtColor(fgmask,cv2.COLOR_GRAY2BGR)
+      draw = fgmask
+     # ret,thresh = cv2.threshold(fgmask,127,255,0)
+      contours,hierarchy = cv2.findContours(fgmask, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+ 
+      for cnt in contours:
+         print(cv2.contourArea(cnt))
+         if(1500< cv2.contourArea(cnt) < 30000 ):
+            x,y,w,h = cv2.boundingRect(cnt)
+            frame=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+            print('found countours2')
+         #else:
+            #frame=cv_image
+
+      #cv2.imshow("winname2", frame)
+      #c = cv2.waitKey(1)
+      # cnt = contours[0]
+        #    x,y,w,h = cv2.boundingRect(cnt)
+        # cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),5)
+         #cv2.imshow(winname, draw)
+         #cv2.imshow(winname2, frame)
+        #c = cv2.waitKey(1)
+        #count=count+1
+      if not(count % 4):
+         bgs_mog = cv2.BackgroundSubtractorMOG2(50,20,True)
+      return frame
+
+
    def callback(self,data):
        try:
          cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
@@ -68,6 +110,9 @@ class ImageConverter:
             output_img=self.FarnebackAlgorithm(self.grayscale_prev_img,grayscale_next_img,cv_image)
             self.grayscale_prev_img=grayscale_next_img
             self.output_image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"));
+         elif self.mode == 2:
+            output_img=self.MOG2(cv_image)
+            self.output_image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))  
        except CvBridgeError as e:
          print(e)
    
